a6f7cd95db0ccf3ae06cd3353b5da7af288f7d5d
Thomas <tienhau.it@gmail.com>
2024-11-08 22:07:44 +0700

    Create: Home Page Interface Base Creation Exercise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
 import Link from "next/link";
 import styles from "@/styles/app.module.css";
+import BasicTable from "@/components/BasicTable";
 
 export default function Home() {
-  return (
-    <ul>
-      <li>
-        <Link href={"/facebook"} className={styles['red']}>Facebook</Link>
-      </li>
-      <li>
-        <Link href="/youtube">Youtube</Link>
-      </li>
-      <li>
-        <Link href="/google">Google</Link>
-      </li>
-    </ul>
-  )
+    return (
+        <>
+            <ul>
+                <li>
+                    <Link href={"/facebook"} className={styles["red"]}>
+                        Facebook
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/youtube">Youtube</Link>
+                </li>
+                <li>
+                    <Link href="/google">Google</Link>
+                </li>
+            </ul>
+            <BasicTable/>
+        </>
+    );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <div style={{
+            width: '100%', 
+            height: '50px',
+            backgroundColor: '#ccc', 
+            textAlign: 'center',
+            alignContent: 'center'
+        }}>
+            Next.JS Basics with Eric - HoiDanIT
+        </div>
+    );
+}
+
+export default Footer;
